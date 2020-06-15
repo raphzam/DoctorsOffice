d51da8b7efd25ddb051bac422f0a614f60395f5c
raphzam <raphaeljasonzamora@gmail.com>
2020-06-15 16:59:20 -0400

    added comments

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Declare variables
         String patientName = "Stacey Manson";
         boolean hasInsurance = false;
         double temp = 102.2;
         int numVisits = 5;
+        int coPay = 25;
+        String nextAppointment = "6/20/20";
+        String cellNum = "301 - 555 - 5555";
 
+        //print message and call variables
         System.out.println("Patient Details:");
         System.out.println("Name: " + patientName);
         System.out.println("Patient has insurance: " + hasInsurance);
         System.out.println("Body temperature: " + temp );
         System.out.println("Number of visits: " + numVisits);
+        System.out.println("Copay amount: $" + coPay);
+        System.out.println("Next appointment: " + nextAppointment);
+        System.out.println("Contact number: " + cellNum);
+
     }
 
 }
